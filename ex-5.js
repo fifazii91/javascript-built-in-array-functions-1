6886804a6fc9818918bc84bc9058a93a53389756
Orayanee Janjaeng <orayaneejj@gmail.com>
2024-10-09 13:18:10 +0700

    answer: ex1-5

--- ex-5.js
+++ ex-5.js
@@ -7,6 +7,11 @@ const students = [
 
 function getAverageStudentScore(students) {
   // Start coding here
+  return (
+    students
+      .map((student) => student.score)
+      .reduce((sum, score) => (sum += score), 0) / students.length
+  );
 }
 
-getAverageStudentScore(students); // Output: 87.5
+console.log(getAverageStudentScore(students)); // Output: 87.5
